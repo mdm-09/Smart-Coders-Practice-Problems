1c5537636df4c12ace78fed4ff6cb341f660c564
mdm-09 <mdmuneebuddindk@gmail.com>
2026-09-07 14:41:12 +0530

    Add files via upload

---
+++ Problem 1.py
@@ -0,0 +1,13 @@
+#Read two Numbers and print their sum, difference, product, quotient, and remainder 
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+sum=num1+num2
+difference=num1-num2
+product=num1*num2
+quotient=num1/num2
+remainder=num1%num2
+print("sum:",sum)
+print("difference:",difference)
+print("product:",product)
+print("quotient:",quotient)
+print("remainder:",remainder)
